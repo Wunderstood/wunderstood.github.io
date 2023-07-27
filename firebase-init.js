8d019da189555a9c69aa59dc30d5b8bce9ba2881
Wunderstood <140664243+Wunderstood@users.noreply.github.com>
2023-07-27 15:40:55 -0700

    Firebase config for login

---
+++ firebase-init.js
@@ -0,0 +1,14 @@
+// firebase-init.js
+// Replace with your actual Firebase config (from the Firebase Console)
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
