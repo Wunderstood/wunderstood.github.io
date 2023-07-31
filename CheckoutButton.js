e5494bf04ba068533a4b3bcc13620bfcb581b01b
Wunderstood <140664243+Wunderstood@users.noreply.github.com>
2023-07-31 01:37:45 -0700

    adding logging to checkout button

--- CheckoutButton.js
+++ CheckoutButton.js
@@ -9,10 +9,10 @@ async function checkout(event) {
 
     // Get the uuid from the cookie
     const uuid = Cookies.get('uuid');
+    console.log('UUID:', uuid);
 
     // Fetch the product ID from the clicked button's data attribute
     const productId = event.target.dataset.productId; 
-
     console.log('Product ID:', productId);
 
     // Make a request to your server to create a Checkout Session
@@ -27,13 +27,17 @@ async function checkout(event) {
         })
     })
     .then(function(response) {
+        console.log('Response from server:', response);
         return response.json();
     })
     .then(function(session) {
+        console.log('Checkout session:', session);
         return stripe.redirectToCheckout({ sessionId: session.id });
     })
     .then(function(result) {
+        console.log('Stripe result:', result);
         if (result.error) {
+            console.error('Stripe error:', result.error.message);
             alert(result.error.message);
         }
     })
