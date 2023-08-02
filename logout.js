fd6d17ed72b4ec14aa160b3f93c98c13d70ba740
Wunderstood <140664243+Wunderstood@users.noreply.github.com>
2023-08-02 15:32:20 -0700

    deleting all cookies

--- logout.js
+++ logout.js
@@ -2,12 +2,11 @@ window.onload = function() {
     document.getElementById('logoutButton').addEventListener('click', function() {
         console.log("Logout button clicked");  // Debugging line
         // Deleting cookies
-        document.cookie = 'uuid=; Max-Age=0; path=/; domain=wunderstood.com';
-        document.cookie = 'pro=; Max-Age=0; path=/; domain=wunderstood.com';
-        document.cookie = 'reportBalance=; Max-Age=0; path=/; domain=wunderstood.com';
-        document.cookie = 'summaryBalance=; Max-Age=0; path=/; domain=wunderstood.com';
-        document.cookie = 'sessionId=; Max-Age=0; path=/; domain=wunderstood.com';
-
+        document.cookie.split(";").forEach((c) => {
+            document.cookie = c
+                .replace(/^ +/, "")
+                .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+        });
         // Redirecting to wunderstood.com home page
         window.location.href = "https://auth.wunderstood.com/logout";
     });
