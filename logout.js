cc480f9ffcb8d492a8db8dcf8b11390847c9d43a
Wunderstood <140664243+Wunderstood@users.noreply.github.com>
2023-08-02 15:23:04 -0700

    fix clickability on login

--- logout.js
+++ logout.js
@@ -1,11 +1,14 @@
-document.getElementById('logoutButton').addEventListener('click', function() {
-    // Deleting cookies
-    document.cookie = 'uuid=; Max-Age=0; path=/; domain=wunderstood.com';
-    document.cookie = 'pro=; Max-Age=0; path=/; domain=wunderstood.com';
-    document.cookie = 'reportBalance=; Max-Age=0; path=/; domain=wunderstood.com';
-    document.cookie = 'summaryBalance=; Max-Age=0; path=/; domain=wunderstood.com';
-    document.cookie = 'sessionId=; Max-Age=0; path=/; domain=wunderstood.com';
+window.onload = function() {
+    document.getElementById('logoutButton').addEventListener('click', function() {
+        console.log("Logout button clicked");  // Debugging line
+        // Deleting cookies
+        document.cookie = 'uuid=; Max-Age=0; path=/; domain=wunderstood.com';
+        document.cookie = 'pro=; Max-Age=0; path=/; domain=wunderstood.com';
+        document.cookie = 'reportBalance=; Max-Age=0; path=/; domain=wunderstood.com';
+        document.cookie = 'summaryBalance=; Max-Age=0; path=/; domain=wunderstood.com';
+        document.cookie = 'sessionId=; Max-Age=0; path=/; domain=wunderstood.com';
 
-    // Redirecting to wunderstood.com home page
-    window.location.href = "https://auth.wunderstood.com/logout";
-});
+        // Redirecting to wunderstood.com home page
+        window.location.href = "https://auth.wunderstood.com/logout";
+    });
+}
