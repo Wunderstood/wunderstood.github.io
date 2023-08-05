6cd72d5f8df26d2cd4775beb3d3c52cf885e4493
Wunderstood <140664243+Wunderstood@users.noreply.github.com>
2023-08-04 22:03:40 -0700

    updating stripe to live

--- CheckoutButton.js
+++ CheckoutButton.js
@@ -1,5 +1,5 @@
 // Stripe instance
-var stripe = Stripe('pk_test_51NYO3sCrnQSygLy6qcWk0Iy7uVhmwxzPG35y6LVaZA9AFQZro6pjHi4ILPJmWRbL1XvYPz6mgkW0E7PK0z6lXzpa003YXgSZwv');
+var stripe = Stripe('pk_live_51NYO3sCrnQSygLy6Ei4SZIYeacRjgcSu8H20vvNV6zuBpBaLvv1lt4l2gZ3OMVgaOS87vE3xKjHcg5vRIhWV263j00C6yiJYYU');
 
 // Function to get cookie by name
 function getCookie(name) {
